b0b0364f8640c0af99c72748043a4c566d620057
Josh Beatty SurfaceX <joshuabeatty88@gmail.com>
2020-09-07 01:44:17 -0600

    Start the mix_column method

--- transformation.py
+++ transformation.py
@@ -1,3 +1,8 @@
+from finite_field_arithmetic import AESFiniteField
+
+ff = AESFiniteField()
+
+
 class AESTransform:
     def sub_bytes(self):
         pass
@@ -5,7 +10,24 @@ class AESTransform:
     def shift_rows(self):
         pass
 
-    def mix_columns(self):
+    def mix_columns(self, s):
+        # Operate on the State <s> col-by-col
+        # Treat each col as a four-term polynomial
+        # Col is considered as polynomials over GF(2^8) and multiplied modulo x^4 + 1 with a(x)
+        # a(x) = {03}x^3 + {01}x^2 + {01}x + {02}
+        # s'(x) = a(x) x s(x)
+        for col in s:
+            self.mix_column(col)
+        pass
+
+    def mix_column(self, col):
+        # We need to calculate each element in col using all elements in col
+        nb = 4
+        for i, r in enumerate(col):
+            col[i] = ff.add(
+                1, 2
+            )
+
         pass
 
     def add_round_key(self):
