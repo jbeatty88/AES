30de29739e81c71e5ac4ff5dd93b72e00134de62
Josh <joshuabeatty88@gmail.com>
2020-09-08 01:28:22 -0600

    Add functional finite field multiply method

--- finite_field_arithmetic.py
+++ finite_field_arithmetic.py
@@ -26,7 +26,7 @@ class AESFiniteField:
 
     mx = 0x1b
 
-    def add(self, a1, a2):
+    def add(self, a1: int, a2: int) -> int:
         """Performs finite field addition of two integers.
 
         If a1 and a2 aren't integers, try to cast into base16 integer.
@@ -49,15 +49,9 @@ class AESFiniteField:
         :return: a1 XOR a2
         """
 
-        try:
-            a1_int = int(a1, 16)
-            a2_int = int(a2, 16)
-        except:
-            a1_int = a1
-            a2_int = a2
-        return hex(a1_int ^ a2_int)  # FF addition and subtraction with XOR
+        return a1 ^ a2  # FF addition and subtraction with XOR
 
-    def xtime(self, n):
+    def xtime(self, n: int) -> int:
         """Performs finite field multiplication by x.
 
         Multiplying the binary polynomial (b) by polynomial x results in
@@ -81,20 +75,53 @@ class AESFiniteField:
         """
         p = n << 1  # Multiply n by x (ie 00000010 or 02)
         if bu.get_msb(p) == 128:  # If the MSB is the 8th bit
-            return hex(p)  # Return because we're already reduced.
+            return p  # Return because we're already reduced.
         else:
             p1 = p ^ self.mx  # Reduce by XOR the polynomial m(x) = x^8 + x^4 + x^3 + x + 1
-            return hex(bu.clear_k_bit(9, p1))  # Drop the 9th bit and return
+            return bu.clear_k_bit(9, p1)  # Drop the 9th bit and return
 
-    def multiply(self, f1, f2):
+    def multiply(self, f1: int, f2: int) -> int:
         """Performs finite filed multiplication between two numbers.
 
         Multiplication in GF(2^8) corresponds  with  the multiplication of polynomials
         modulo an irreducible polynomial of degree 8 (i.e. mx(x)). This can be implemented
-        using xtime() and adding intermediate results
+        using xtime() and adding intermediate results.
 
         :param f1: factor1
         :param f2: factor2
         :return: f1 * f2 GF(2^8)
         """
-        return f1 * f2
+
+        # Make the table with all the xtime values for f1
+        xtime_table = self.make_xtime_table(f1, f2)
+        product = 0
+        # Check each bit of f2
+        for k in range(8):
+            # If k bit of f2 is set, that represent a polynomial value of x^k
+            if bu.kth_bit_set(k, f2):
+                # Add it to the product
+                product = self.add(product, xtime_table[k])
+        # Return the produc
+        return product
+
+    def make_xtime_table(self, f1: int, f2: int) -> list:
+        # Xtime table will have 8 elements, corresponding with 8 bits
+        xtime_table = [0] * 8
+        # First element is f1 multiplicand
+        xtime_table[0] = f1
+        p = f1
+        # Keep track of where to place the xtime result in the table
+        idx = 1
+        # Compute Xtime until f2 multiplier can't be divided anymore
+        while f2 != 1:
+            # Compute xtime of number
+            p = self.xtime(p)
+            # Add it to table at correct index
+            xtime_table[idx] = p
+            # Increment index
+            idx += 1
+            # Divide f2
+            f2 >>= 1
+        # Return the xtime table
+        return xtime_table
+
