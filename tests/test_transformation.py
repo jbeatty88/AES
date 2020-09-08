9d0641870ff8aa5fcd36cea09985583722117b75
Josh <joshuabeatty88@gmail.com>
2020-09-07 20:05:29 -0600

    Update mix_column method and tests for it

--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -1,2 +1,25 @@
-def test_sub_bytes():
-    assert False
+from transformation import AESTransform
+import numpy as np
+t = AESTransform()
+
+state_mat_hex = [
+    [0xae, 0xde, 0xde, 0xda],
+    [0x3e, 0xd0, 0xad, 0xbe],
+    [0xa7, 0x0d, 0xef, 0xde],
+    [0xdf, 0xf4, 0xad, 0xbe],
+]
+state_mat_int = np.array([
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+])
+
+state_mat_int_col1 = np.array([[0, 4, 8, 12]])
+
+def test_mix_columns():
+    t.mix_columns(state_mat_int)
+    assert True
+
+def test_mix_column():
+    b = t.mix_column(state_mat_int_col1)
--- transformation.py
+++ transformation.py
@@ -1,9 +1,17 @@
 from finite_field_arithmetic import AESFiniteField
+import numpy as np
 
 ff = AESFiniteField()
 
 
 class AESTransform:
+    ax = np.array([  # a(x) = {03}x^3 + {01}x^2 + {01}x + {02}
+        [0x02, 0x03, 0x01, 0x01],
+        [0x01, 0x02, 0x03, 0x01],
+        [0x01, 0x01, 0x02, 0x03],
+        [0x03, 0x01, 0x01, 0x02],
+    ])
+
     def sub_bytes(self):
         pass
 
@@ -14,21 +22,39 @@ class AESTransform:
         # Operate on the State <s> col-by-col
         # Treat each col as a four-term polynomial
         # Col is considered as polynomials over GF(2^8) and multiplied modulo x^4 + 1 with a(x)
-        # a(x) = {03}x^3 + {01}x^2 + {01}x + {02}
-        # s'(x) = a(x) x s(x)
-        for col in s:
-            self.mix_column(col)
+        for col in range(4):
+            self.mix_column(s[:, col])  # Calculate value for each element in col
+            # Apply that back to the original state array
         pass
 
     def mix_column(self, col):
+        byte = []
+        # Unpack the state column values
+        s0, s1, s2, s3 = col[0]
+        # s0, s1, s2, s3 = map(int, col[0])
+        print(s0)
+        print(s1)
+        print(s2)
+        print(s3)
         # We need to calculate each element in col using all elements in col
-        nb = 4
-        for i, r in enumerate(col):
-            col[i] = ff.add(
-                1, 2
+        for i in range(4):
+            # s'(x) = a(x) x s(x)
+            a0, a1, a2, a3 = self.ax[i]
+            # a0, a1, a2, a3 = map(int, self.ax[i])
+            byte.append(
+                ff.add(
+                    ff.add(
+                        ff.multiply(s0, a0),
+                        ff.multiply(s1, a1)
+                    ),
+                    ff.add(
+                        ff.multiply(s2, a2),
+                        ff.multiply(s3, a3)
+                    )
+                )
             )
-
-        pass
+        print(byte)
+        return byte
 
     def add_round_key(self):
         pass
