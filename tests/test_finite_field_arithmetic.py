f57952948bc580c0eba4b903af96042e6ccb0c11
Josh <joshuabeatty88@gmail.com>
2020-09-08 01:28:41 -0600

    Test finite field multiply method

--- tests/test_finite_field_arithmetic.py
+++ tests/test_finite_field_arithmetic.py
@@ -4,15 +4,15 @@ ff = AESFiniteField()
 
 
 def test_add():
-    assert ff.add(0x57, 0x83) == hex(0xd4)
+    assert hex(ff.add(0x57, 0x83)) == hex(0xd4)
 
 
 def test_xtime():
-    assert ff.xtime(0x57) == hex(0xae)
-    assert ff.xtime(0xae) == hex(0x47)
-    assert ff.xtime(0x47) == hex(0x8e)
-    assert ff.xtime(0x8e) == hex(0x07)
+    assert hex(ff.xtime(0x57)) == hex(0xae)
+    assert hex(ff.xtime(0xae)) == hex(0x47)
+    assert hex(ff.xtime(0x47)) == hex(0x8e)
+    assert hex(ff.xtime(0x8e)) == hex(0x07)
 
 
 def test_ff_multiply():
-    assert ff.multiply(5, 5) == 25
+    assert hex(ff.multiply(0x57, 0x13)) == hex(0xfe)
