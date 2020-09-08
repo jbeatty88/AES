c0319779cb4dc8bae0bdde775046f64c9de7e1c5
Josh <joshuabeatty88@gmail.com>
2020-09-07 20:06:04 -0600

    Update test for xtime method

--- tests/test_finite_field_arithmetic.py
+++ tests/test_finite_field_arithmetic.py
@@ -1,12 +1,15 @@
 from finite_field_arithmetic import AESFiniteField
+
 ff = AESFiniteField()
+
+
 def test_add():
     assert ff.add(0x57, 0x83) == hex(0xd4)
 
 
 def test_xtime():
     assert ff.xtime(0x57) == hex(0xae)
-    assert ff.xtime( 0xae) == hex(0x47)
+    assert ff.xtime(0xae) == hex(0x47)
     assert ff.xtime(0x47) == hex(0x8e)
     assert ff.xtime(0x8e) == hex(0x07)
 
