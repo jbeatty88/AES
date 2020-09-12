7f952ecdbb932c64ff163afd20c9436851ab1e21
Josh <joshuabeatty88@gmail.com>
2020-09-11 20:46:06 -0600

    Copy all prior tests to single test class

--- never_use_this_py_aes.py
+++ never_use_this_py_aes.py
@@ -4,7 +4,40 @@ from utils import get_msb, clear_k_bit, bit_count, kth_bit_set
 
 
 class PyAES:
+
+    def __init__(self, message, key=None):
+        self.message = message
+        self.key = key
+        if key is None:
+            self.mode = 'encrypt'
+        else:
+            self.mode = 'decrypt'
+
+    sbox = [
+        0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+        0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+        0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+        0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+        0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+        0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+        0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+        0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+        0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+        0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+        0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+        0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+        0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+        0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+        0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+        0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16]
+
     mx = 0x1b
+    ax = np.array([  # a(x) = {03}x^3 + {01}x^2 + {01}x + {02}
+        [0x02, 0x03, 0x01, 0x01],
+        [0x01, 0x02, 0x03, 0x01],
+        [0x01, 0x01, 0x02, 0x03],
+        [0x03, 0x01, 0x01, 0x02],
+    ])
 
     def ff_add(self, a1: int, a2: int) -> int:
         """Performs finite field addition of two integers.
@@ -60,36 +93,6 @@ class PyAES:
             p1 = p ^ self.mx  # Reduce by XOR the polynomial m(x) = x^8 + x^4 + x^3 + x + 1
             return clear_k_bit(9, p1)  # Drop the 9th bit and return
 
-    def ff_multiply(self, f1: int, f2: int) -> int:
-        """Performs finite filed multiplication between two numbers.
-
-        Multiplication in GF(2^8) corresponds  with  the multiplication of polynomials
-        modulo an irreducible polynomial of degree 8 (i.e. mx(x)). This can be implemented
-        using xtime() and adding intermediate results.
-
-        :param f1: factor1
-        :param f2: factor2
-        :return: f1 * f2 GF(2^8)
-        """
-
-        # Optimized by choosing the factor with fewer bytes as multiplier
-        if bit_count(f1) < bit_count(f2):
-            f1 = f1 ^ f2
-            f2 = f1 ^ f2
-            f1 = f1 ^ f2
-
-        # Make the table with all the xtime values for f1
-        xtime_table = self.make_xtime_table(f1, f2)
-        product = 0
-        # Check each bit of f2
-        for k in range(8):
-            # If k bit of f2 is set, that represent a polynomial value of x^k
-            if kth_bit_set(k, f2):
-                # Add it to the product
-                product = self.ff_add(product, xtime_table[k])
-        # Return the product
-        return product
-
     def make_xtime_table(self, f1: int, f2: int) -> list:
         """Creates a table of xtime results.
 
@@ -121,12 +124,35 @@ class PyAES:
         # Return the xtime table
         return xtime_table
 
-    ax = np.array([  # a(x) = {03}x^3 + {01}x^2 + {01}x + {02}
-        [0x02, 0x03, 0x01, 0x01],
-        [0x01, 0x02, 0x03, 0x01],
-        [0x01, 0x01, 0x02, 0x03],
-        [0x03, 0x01, 0x01, 0x02],
-    ])
+    def ff_multiply(self, f1: int, f2: int) -> int:
+        """Performs finite filed multiplication between two numbers.
+
+        Multiplication in GF(2^8) corresponds  with  the multiplication of polynomials
+        modulo an irreducible polynomial of degree 8 (i.e. mx(x)). This can be implemented
+        using xtime() and adding intermediate results.
+
+        :param f1: factor1
+        :param f2: factor2
+        :return: f1 * f2 GF(2^8)
+        """
+
+        # Optimized by choosing the factor with fewer bytes as multiplier
+        if bit_count(f1) < bit_count(f2):
+            f1 = f1 ^ f2
+            f2 = f1 ^ f2
+            f1 = f1 ^ f2
+
+        # Make the table with all the xtime values for f1
+        xtime_table = self.make_xtime_table(f1, f2)
+        product = 0
+        # Check each bit of f2
+        for k in range(8):
+            # If k bit of f2 is set, that represent a polynomial value of x^k
+            if kth_bit_set(k, f2):
+                # Add it to the product
+                product = self.ff_add(product, xtime_table[k])
+        # Return the product
+        return product
 
     def sub_bytes(self):
         """
@@ -155,31 +181,6 @@ class PyAES:
         :return:
         """
         pass
-
-    def mix_columns(self, s):
-        """Transforms the state matrix column-by-column.
-
-        The mix_columns() transformation operates on the State
-        column-by-column, treating each column as a four-term
-        polynomial.
-
-        Columns are considered as polynomial over
-        GF(2^8) and multiplied modulo x^4 + 1 with a(x) where
-        a(x) = {03}x^3 + {01}x^2 + {01}x + {02}
-
-        This can be seen as matrix multiplication; let
-            s'(x) = a(x) ffmult s(x)
-
-        :param s: 4 x 4 state matrix
-        :return: None, modifies original matrix to be s'(x)
-        """
-
-        for col in range(4):
-            # Calculate value for each element in s column and return s' column
-            s_col = s[:, col]
-            s_p_col = self.mix_column(s_col)
-            # Replace s column with s' column
-            s[:, col] = s_p_col
 
     def mix_column(self, col):
         """Transforms each column item by multiply modulo x4 + 1 with a(x)
@@ -216,6 +217,31 @@ class PyAES:
                 )
             )
         return transformed_bytes
+
+    def mix_columns(self, s):
+        """Transforms the state matrix column-by-column.
+
+        The mix_columns() transformation operates on the State
+        column-by-column, treating each column as a four-term
+        polynomial.
+
+        Columns are considered as polynomial over
+        GF(2^8) and multiplied modulo x^4 + 1 with a(x) where
+        a(x) = {03}x^3 + {01}x^2 + {01}x + {02}
+
+        This can be seen as matrix multiplication; let
+            s'(x) = a(x) ffmult s(x)
+
+        :param s: 4 x 4 state matrix
+        :return: None, modifies original matrix to be s'(x)
+        """
+
+        for col in range(4):
+            # Calculate value for each element in s column and return s' column
+            s_col = s[:, col]
+            s_p_col = self.mix_column(s_col)
+            # Replace s column with s' column
+            s[:, col] = s_p_col
 
     def add_round_key(self):
         """
--- tests/test_never_use_this_py_aes.py
+++ tests/test_never_use_this_py_aes.py
@@ -1,2 +1,91 @@
+from never_use_this_py_aes import PyAES
+
+aes = PyAES("plaintext message", "key")
+aes_decrypt = PyAES("10e88c420358a84775422ca05677891f", "a38130d79493f583b6543e4e0e4b17c9")
+aes_encrypt = PyAES("10e88c420358a84775422ca05677891f")
+
+
+def test_can_get_message():
+    assert aes.message == "plaintext message"
+    assert aes_decrypt.message == "10e88c420358a84775422ca05677891f"
+    assert aes_encrypt.message == "10e88c420358a84775422ca05677891f"
+
+
+def test_can_get_key():
+    assert aes.key == "key"
+    assert aes_decrypt.key == "a38130d79493f583b6543e4e0e4b17c9"
+    assert aes_encrypt.key == None
+
+
+def test_mode_set_correctly():
+    assert aes.mode == "decrypt"
+    assert aes_decrypt.mode == "decrypt"
+    assert aes_encrypt.mode == "encrypt"
+
+
 def test_ff_add():
+    assert hex(aes.ff_add(0x57, 0x83)) == hex(0xd4)
+
+
+def test_xtime():
+    assert hex(aes.xtime(0x57)) == hex(0xae)
+    assert hex(aes.xtime(0xae)) == hex(0x47)
+    assert hex(aes.xtime(0x47)) == hex(0x8e)
+    assert hex(aes.xtime(0x8e)) == hex(0x07)
+
+
+def test_make_xtime_table():
+    assert False
+
+
+def test_ff_multiply():
+    assert hex(aes.ff_multiply(0x57, 0x13)) == hex(0xfe)
+    assert hex(aes.ff_multiply(0x13, 0x57)) == hex(0xfe)
+    assert hex(aes.ff_multiply(0x70, 0x27)) == hex(0xc9)
+    assert hex(aes.ff_multiply(0x27, 0x70)) == hex(0xc9)
+    assert hex(aes.ff_multiply(0x22, 0x0e)) == hex(0xc7)
+    assert hex(aes.ff_multiply(0x0e, 0x22)) == hex(0xc7)
+
+
+def test_sub_bytes():
+    assert False
+
+
+def test_shift_rows():
+    assert False
+
+
+def test_mix_column():
+    assert False
+
+
+def test_mix_columns():
+    assert False
+
+
+def test_add_round_key():
+    assert False
+
+
+def test_inv_sub_bytes():
+    assert False
+
+
+def test_inv_shift_rows():
+    assert False
+
+
+def test_inv_mix_column():
+    assert False
+
+
+def test_inv_mix_columns():
+    assert False
+
+
+def test_sub_word():
+    assert False
+
+
+def test_rot_word():
     assert False
